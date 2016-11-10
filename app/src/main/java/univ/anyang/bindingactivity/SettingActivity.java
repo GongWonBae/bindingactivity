9051a3f15adf52c8ea8a16a46d5cace3a07ab718
taeoki <kimeee45@gmail.com>
2016-11-10 09:34:35 +0900

    세팅창 완성해쪄용 뿌잉뿌잉 (feat 원배 - 깃 푸시좀 잘해줘요 뿌잉뿌잉)

--- app/src/main/java/univ/anyang/bindingactivity/SettingActivity.java
+++ app/src/main/java/univ/anyang/bindingactivity/SettingActivity.java
@@ -29,7 +29,7 @@ public class SettingActivity extends AppCompatActivity {
 
                 } else          //false일 때
                 {
-                    
+
                 }
             }
         });
