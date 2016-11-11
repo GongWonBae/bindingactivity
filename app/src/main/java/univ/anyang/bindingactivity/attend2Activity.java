5f34a4994352be56b2269e4515c2c0999949919b
taeoki <kimeee45@gmail.com>
2016-11-11 09:46:30 +0900

    충돌해결 머지용

--- app/src/main/java/univ/anyang/bindingactivity/attend2Activity.java
+++ app/src/main/java/univ/anyang/bindingactivity/attend2Activity.java
@@ -82,26 +82,32 @@ public class attend2Activity extends Activity {
                             }
                             else if(aJp.AttendFlag.equals("10"))
                             {   // 유고처리됨
-
+                                txtview_attend.setText("유고처리됨 확인되었습니다.");
                             }
                             else if(aJp.AttendFlag.equals("11"))
                             {   // 결석처리됨
-
+                                txtview_attend.setText("결석처리 확인되었습니다.");
+                            }
+                            else if(aJp.AttendFlag.equals(null))
+                            {   // 아직 출석처리 되지 않았습니다 다시 시도하세요.
+                                txtview_attend.setText("아직 출석처리 되지 않았습니다 다시 시도하세요..");
                             }
                         }
                         else if(aJp.BeaconFlag.equals("1"))     // 거리밖에 비콘 감지
                         {
+                            // 강의실(비콘)이 너무 멀리 있습니다 다시 시도하세요
                             btn_ok.setVisibility(View.INVISIBLE);
                             btn_cancel.setVisibility(View.INVISIBLE);
                             btn_close.setVisibility(View.VISIBLE);
-                            txtview_attend.setText("강의실과 너무 멀리 있습니다.");
+                            txtview_attend.setText("Beacon Flag : 1 \n강의실(비콘)이 너무 멀리 있습니다 다시 시도하세요");
                         }
                         else if(aJp.BeaconFlag.equals("2"))     // 비콘을 찾을 수 없음
                         {
+                            // 강의실(비콘)을 찾을 수 없습니다 다시 시도하세요.
                             btn_ok.setVisibility(View.INVISIBLE);
                             btn_cancel.setVisibility(View.INVISIBLE);
                             btn_close.setVisibility(View.VISIBLE);
-                            txtview_attend.setText("위치를 확인할 수 없습니다.");
+                            txtview_attend.setText("Beacon Flag : 2 \n 강의실(비콘)을 찾을 수 없습니다 다시 시도하세요.");
                         }
                         break;
                 }
